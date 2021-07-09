a410a262f7aff3cb4a7ac99132d5c4c6c31bf2b9
Cooper Saye <coopersaye@gmail.com>
2021-07-09 11:45:33 -0700

    Initialize firebase.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,23 @@
 import './App.css';
 
-function App() {
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { firebaseConfig } from '../firebaseConfig.js';
+
+import styles from './App.module.css';
+
+// initialize firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export default function App() {
+  useAuthState(firebase.auth());
+
   return (
     <div className="App">
     </div>
   );
 }
-
-export default App;
