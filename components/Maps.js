39b383153d29d66ed6b15c472d2b1eca61bdc66e
Cooper Saye <coopersaye@gmail.com>
2021-07-29 21:42:49 -0700

    Create tile picker.

--- components/Maps.js
+++ components/Maps.js
@@ -9,24 +9,28 @@ const defaultColors = [
   '#bbbbbb', '#aaaaaa', '#999999', '#888888',
   '#777777', '#666666', '#555555', '#444444',
   '#333333', '#222222', '#111111', '#000000'
-]
+];
+
+const defaultTiles = Array(16).fill(Array(16).fill(0));
 
 export default function Maps() {
   const [colors, setColors] = useState(defaultColors);
   const [currColor, setCurrColor] = useState(0);
+  const [tiles, setTiles] = useState(defaultTiles);
+  const [currTile, setCurrTile] = useState(0);
 
   return (
     <div className={styles.container}>
       <h1>Colors</h1>
-      <div className={styles.colorgrid}>
+      <div className={styles.tilegrid}>
         {
           colors.map((color, i) =>
             <div
               onClick={() => setCurrColor(i)}
               className={
                 currColor === i ?
-                `${styles.colorinput} ${styles.selected}` :
-                styles.colorinput
+                `${styles.tile} ${styles.selected}` :
+                styles.tile
               }
               key={`${i}`}
               style={{ background: color }}
@@ -44,6 +48,24 @@ export default function Maps() {
           setColors(newColors);
         }}
       />
+      <h1>Tiles</h1>
+      <div className={styles.tilegrid}>
+        {
+          tiles.map((tile, i) =>
+            <div
+              onClick={() => setCurrTile(i)}
+              className={
+                currTile === i ?
+                `${styles.tile} ${styles.selected}` :
+                styles.tile
+              }
+              key={`${i}`}
+              style={{ background: colors[tiles[0][0]] }}
+            >
+            </div>
+          )
+        }
+      </div>
     </div>
   );
 }
