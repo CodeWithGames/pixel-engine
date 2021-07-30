29e294a2edd3804ecacbde8a5e12d59b8c47d14b
Cooper Saye <coopersaye@gmail.com>
2021-07-30 14:28:06 -0700

    Update map units.

--- components/Maps.js
+++ components/Maps.js
@@ -24,7 +24,12 @@ const defaultColors = [
 const defaultTiles = Array(16).fill(Array(tileGridSize ** 2).fill(0));
 const defaultMaps = Array(16).fill(Array(mapGridSize ** 2).fill(0));
 
-export default function Maps() {
+export default function Maps(props) {
+  const { tileSize, mapSize } = props;
+  const tilePixels = tileGridPixels * tileSize;
+  const mapPixels = mapGridPixels * mapSize;
+  const pixelPixels = Math.floor(mapGridPixels / tileSize);
+
   const [colors, setColors] = useState(defaultColors);
   const [currColor, setCurrColor] = useState(0);
   const [tiles, setTiles] = useState(defaultTiles);
@@ -39,14 +44,14 @@ export default function Maps() {
   function drawTile() {
     // clear canvas
     tileCtx.fillStyle = gridColor;
-    tileCtx.fillRect(0, 0, tileSize, tileSize);
+    tileCtx.fillRect(0, 0, tilePixels, tilePixels);
     // get current tile
     const tile = tiles[currTile];
     // for each pixel
-    for (let y = 0; y < tileGridSize; y++) {
-      for (let x = 0; x < tileGridSize; x++) {
+    for (let y = 0; y < tileSize; y++) {
+      for (let x = 0; x < tileSize; x++) {
         // set fill color
-        const colorIndex = y * tileGridSize + x;
+        const colorIndex = y * tileSize + x;
         const color = colors[tile[colorIndex]];
         tileCtx.fillStyle = color;
         // set fill position and size
@@ -71,33 +76,33 @@ export default function Maps() {
   function drawMap() {
     // clear canvas
     mapCtx.fillStyle = gridColor;
-    mapCtx.fillRect(0, 0, mapSize, mapSize);
+    mapCtx.fillRect(0, 0, mapPixels, mapPixels);
     // get current map
     const map = maps[currMap];
     // for each tile
-    for (let y = 0; y < mapGridSize; y++) {
-      for (let x = 0; x < mapGridSize; x++) {
+    for (let y = 0; y < mapSize; y++) {
+      for (let x = 0; x < mapSize; x++) {
         // get tile
-        const tileIndex = y * mapGridSize + x;
+        const tileIndex = y * mapSize + x;
         const tile = tiles[map[tileIndex]];
         // for each pixel
-        for (let yp = 0; yp < tileGridSize; yp++) {
-          for (let xp = 0; xp < tileGridSize; xp++) {
+        for (let yp = 0; yp < tileSize; yp++) {
+          for (let xp = 0; xp < tileSize; xp++) {
             // set fill color
-            const colorIndex = yp * tileGridSize + xp;
+            const colorIndex = yp * tileSize + xp;
             const color = colors[tile[colorIndex]];
             mapCtx.fillStyle = color;
             // get fill position and size
-            let xm = x * mapGrid + xp * pixelGrid;
-            let ym = y * mapGrid + yp * pixelGrid;
-            let xs = pixelGrid;
-            let ys = pixelGrid;
+            let xm = x * mapGridPixels + xp * pixelPixels;
+            let ym = y * mapGridPixels + yp * pixelPixels;
+            let xs = pixelPixels;
+            let ys = pixelPixels;
             // if map gridded, adjust fill
             if (mapGridded) {
               if (xp === 0) { xm += 1; xs -= 1; }
               if (yp === 0) { ym += 1; ys -= 1; }
-              if (xp === tileGridSize - 1) xs -= 1;
-              if (yp === tileGridSize - 1) ys -= 1;
+              if (xp === tileSize - 1) xs -= 1;
+              if (yp === tileSize - 1) ys -= 1;
             }
             // fill pixel
             mapCtx.fillRect(xm, ym, xs, ys);
@@ -177,17 +182,17 @@ export default function Maps() {
         className={
           tileGridded ? `${styles.canvas} ${styles.gridded}` : styles.canvas
         }
-        width={tileSize}
-        height={tileSize}
+        width={tilePixels}
+        height={tilePixels}
         onMouseDown={e => {
           // get x and y on canvas
           const currX = e.clientX - tileCanvas.offsetLeft + window.scrollX;
           const currY = e.clientY - tileCanvas.offsetTop + window.scrollY;
           // get x and y in tile units
-          const tileX = Math.floor(currX / tileGrid);
-          const tileY = Math.floor(currY / tileGrid);
+          const tileX = Math.floor(currX / tileGridPixels);
+          const tileY = Math.floor(currY / tileGridPixels);
           // set tile at index
-          const tileIndex = tileY * tileGridSize + tileX;
+          const tileIndex = tileY * tileSize + tileX;
           const newTiles = tiles.slice();
           const newTile = newTiles[currTile].slice();
           newTile.splice(tileIndex, 1, currColor);
@@ -227,17 +232,17 @@ export default function Maps() {
         className={
           mapGridded ? `${styles.canvas} ${styles.gridded}` : styles.canvas
         }
-        width={mapSize}
-        height={mapSize}
+        width={mapPixels}
+        height={mapPixels}
         onMouseDown={e => {
           // get x and y on canvas
           const currX = e.clientX - mapCanvas.offsetLeft + window.scrollX;
           const currY = e.clientY - mapCanvas.offsetTop + window.scrollY;
           // get x and y in map units
-          const tileX = Math.floor(currX / mapGrid);
-          const tileY = Math.floor(currY / mapGrid);
+          const tileX = Math.floor(currX / mapGridPixels);
+          const tileY = Math.floor(currY / mapGridPixels);
           // set map at index
-          const mapIndex = tileY * mapGridSize + tileX;
+          const mapIndex = tileY * mapSize + tileX;
           const newMaps = maps.slice();
           const newMap = newMaps[currMap].slice();
           newMap.splice(mapIndex, 1, currTile);
