1a05df25622a36f3ed8a170fd11f4f089c4c2748
Cooper Saye <coopersaye@gmail.com>
2021-07-31 18:12:19 -0700

    Import favicon.

--- pages/_app.js
+++ pages/_app.js
@@ -21,7 +21,10 @@ export default function App({ Component, pageProps }) {
       <Head>
         <title>Pixel Engine</title>
         <meta name="description" content="A minimalist online game engine." />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/manifest.json" />
       </Head>
       <Component {...pageProps} />
     </>
