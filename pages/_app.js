f1a078634c90089684ffc5b50780df46bb61ae6b
Cooper Saye <coopersaye@gmail.com>
2021-08-02 11:27:01 -0700

    Improve styling.

--- pages/_app.js
+++ pages/_app.js
@@ -25,6 +25,7 @@ export default function App({ Component, pageProps }) {
         <link rel="apple-touch-icon" sizes="180x180" href="favicons/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="favicons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="favicons/favicon-16x16.png" />
+        <link href="https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap" rel="stylesheet" />
         <link rel="manifest" href="/manifest.json" />
       </Head>
       <Header />
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Image from 'next/image';
+
 import firebase from 'firebase/app';
 import signInWithGoogle from '../util/signInWithGoogle.js';
 
@@ -6,11 +8,16 @@ import styles from '../styles/Index.module.css';
 export default function Index() {
   return (
     <div className={styles.container}>
-      <h1>Pixel Engine</h1>
-      {
-        !firebase.auth().currentUser &&
-        <button onClick={signInWithGoogle}>Sign in with Google</button>
-      }
+      <div className={styles.centerbox}>
+        <h1>
+          <Image src="/logo.png" width="48" height="48" />
+          Pixel Engine
+        </h1>
+        {
+          !firebase.auth().currentUser &&
+          <button onClick={signInWithGoogle}>Sign in with Google</button>
+        }
+      </div>
     </div>
   );
 }
