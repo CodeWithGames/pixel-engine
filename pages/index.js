877999326d078e049014019ef7ddc569ee12fe5c
Cooper Saye <coopersaye@gmail.com>
2021-08-02 11:11:28 -0700

    Extract signInWithGoogle function.

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,16 @@
 import firebase from 'firebase/app';
+import signInWithGoogle from '../util/signInWithGoogle.js';
 
 import styles from '../styles/Index.module.css';
 
 export default function Index() {
-  // opens google sign in popup
-  function signInWithGoogle() {
-    const provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider);
-  }
-
   return (
     <div className={styles.container}>
       <h1>Pixel Engine</h1>
-      <button onClick={signInWithGoogle}>Sign in with Google</button>
+      {
+        !firebase.auth().currentUser &&
+        <button onClick={signInWithGoogle}>Sign in with Google</button>
+      }
     </div>
   );
 }
