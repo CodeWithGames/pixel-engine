b56679c314d8ed44c4e3703b9f0f6545e1caf782
Cooper Saye <coopersaye@gmail.com>
2021-08-02 11:13:02 -0700

    Improve header styling.

--- components/Header.js
+++ components/Header.js
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import Image from 'next/image';
 
 import firebase from 'firebase/app';
@@ -8,8 +9,13 @@ import styles from '../styles/Header.module.css';
 export default function Header() {
   return (
     <div className={styles.container}>
-      <Image src="/logo.png" width="48" height="48" />
+      <Link href="/">
+        <a className={styles.logolink}>
+          <Image src="/logo.png" width="48" height="48" />
+        </a>
+      </Link>
       <h1>Pixel Engine</h1>
+      <span className={styles.flexfill} />
       {
         firebase.auth().currentUser ?
         <button onClick={() => firebase.auth().signOut()}>Sign Out</button> :
