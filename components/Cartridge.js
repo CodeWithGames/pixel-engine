393f2c4fe51f412c382fe5ce1713b04740f00fc9
Cooper Saye <coopersaye@gmail.com>
2021-08-02 23:44:41 -0700

    Improve cartridge styling.

--- components/Cartridge.js
+++ components/Cartridge.js
@@ -1,14 +1,27 @@
 import Link from 'next/link';
 
+import { useEffect, useState } from 'react';
+import getUsername from '../util/getUsername.js';
+
 import styles from '../styles/Cartridge.module.css';
 
 export default function Cartridge(props) {
-  const { id, title } = props.game;
+  const { id, title, creator } = props.game;
+
+  const [username, setUsername] = useState('');
+
+  // get username on start
+  useEffect(() => {
+    getUsername(creator).then(u => setUsername(u));
+  }, []);
 
   return (
-    <div>
+    <div className={styles.container}>
       <Link href={`/game/${id}`}>
-        <a>{title}</a>
+        <a>
+          <h1>{title}</h1>
+          <p>{username}</p>
+        </a>
       </Link>
     </div>
   );
