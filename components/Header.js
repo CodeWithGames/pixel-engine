89578803e8751c43bdd3f6ed2bfe6ccca13d6af5
Cooper Saye <coopersaye@gmail.com>
2021-08-10 22:57:33 -0700

    Clean up header.

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,7 @@
 import IconButton from '@material-ui/core/IconButton';
 import Tooltip from '@material-ui/core/Tooltip';
-import ExitToAppIcon from '@material-ui/icons/ExitToApp';
-import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
+import GitHubIcon from '@material-ui/icons/GitHub';
+import PersonIcon from '@material-ui/icons/Person';
 import Link from 'next/link';
 import Image from 'next/image';
 
@@ -13,41 +13,27 @@ import styles from '../styles/Header.module.css';
 export default function Header() {
   return (
     <div className={styles.container}>
-      <Link href="/">
-        <a className={styles.logolink}>
-          <Image src="/logo.png" width="48" height="48" />
-        </a>
-      </Link>
+      <Image src="/logo.png" width="48" height="48" />
       <h1>Pixel Engine</h1>
       <span className={styles.flexfill} />
-      <Link href="/games">
-        <a className="url">Games</a>
-      </Link>
-      {
-        firebase.auth().currentUser &&
-        <Link href="/create">
-          <a className="url">Create</a>
-        </Link>
-      }
-      {
-        firebase.auth().currentUser ?
-        <Tooltip title="Sign Out" arrow>
-          <IconButton
-            className={styles.iconbutton}
-            onClick={() => firebase.auth().signOut()}
-          >
-            <ExitToAppIcon />
-          </IconButton>
-        </Tooltip> :
-        <Tooltip title="Sign In" arrow>
-          <IconButton
-            className={styles.iconbutton}
-            onClick={signInWithGoogle}
-          >
-            <PersonOutlineIcon />
-          </IconButton>
-        </Tooltip>
-      }
+      <Tooltip title="GitHub" arrow>
+        <a
+          href="https://github.com/csaye/pixel-engine"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GitHubIcon />
+        </a>
+      </Tooltip>
+      <Tooltip title="Creator" arrow>
+        <a
+          href="https://csaye.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <PersonIcon />
+        </a>
+      </Tooltip>
     </div>
   );
 }
