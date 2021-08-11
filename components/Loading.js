f87ba7e8a2a0ef31095186bccfe8118af99346c9
Cooper Saye <coopersaye@gmail.com>
2021-08-10 23:04:42 -0700

    Remove unused files.

--- components/Loading.js
+++
@@ -1,9 +0,0 @@
-import styles from '../styles/Loading.module.css';
-
-export default function Loading() {
-  return (
-    <div className={styles.container}>
-      <h1>Loading...</h1>
-    </div>
-  );
-}
