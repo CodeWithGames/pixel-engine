b6525ef18c80efa5deb28dcf9b567017c1453b87
Cooper Saye <coopersaye@gmail.com>
2021-08-11 16:31:40 -0700

    Create Docs component.

---
+++ components/Docs.js
@@ -0,0 +1,10 @@
+import styles from '../styles/Docs.module.css';
+
+export default function Docs() {
+  return (
+    <div className={styles.container}>
+      <h1>Docs</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi nec lobortis justo. In ut purus at neque mollis rutrum. Phasellus semper pulvinar mollis. Sed euismod bibendum elit quis scelerisque. Aliquam sollicitudin finibus felis, sed feugiat quam suscipit sit amet. Pellentesque molestie commodo mi. Sed volutpat velit id purus fermentum auctor. Ut condimentum scelerisque blandit.</p>
+    </div>
+  );
+}
